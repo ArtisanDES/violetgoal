7fe5715ebfa5fdc0bb31220e0aec66511b2b33da
ArtisanDES <288281471+ArtisanDES@users.noreply.github.com>
2026-07-31 08:33:59 +0000

    chore: update football data

--- src/generated-fixtures.js
+++ src/generated-fixtures.js
@@ -5,7 +5,7 @@ window.__FIXTURES_DATA__ = {
   "dateTo": "2026-07-31",
   "count": 0,
   "matches": [],
-  "enrichedAt": "2026-07-31T05:48:09.273Z",
+  "enrichedAt": "2026-07-31T08:33:59.250Z",
   "enrichment": {
     "providerOverrides": 0,
     "apiFootball": "synced",
--- src/generated-prediction-snapshots.js
+++ src/generated-prediction-snapshots.js
@@ -1,1 +1,1 @@
-window.__PREDICTION_SNAPSHOTS__ = {"version":1,"snapshots":{},"updatedAt":"2026-07-31T05:48:09.401Z"};
+window.__PREDICTION_SNAPSHOTS__ = {"version":1,"snapshots":{},"updatedAt":"2026-07-31T08:33:59.383Z"};
