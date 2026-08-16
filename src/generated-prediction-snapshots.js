1c8215334d83ec61126db4feef3dad9943534dd9
ArtisanDES <288281471+ArtisanDES@users.noreply.github.com>
2026-08-16 17:11:20 +0000

    chore: update football data

--- src/generated-prediction-snapshots.js
+++ src/generated-prediction-snapshots.js
@@ -1,1 +1,1 @@
-window.__PREDICTION_SNAPSHOTS__ = {"version":1,"snapshots":{},"updatedAt":"2026-08-16T16:47:35.518Z"};
+window.__PREDICTION_SNAPSHOTS__ = {"version":1,"snapshots":{},"updatedAt":"2026-08-16T17:11:19.098Z"};
