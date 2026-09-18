4222f5f8b6c1beec5d4dc7c3513fbf611c662bcd
ArtisanDES <288281471+ArtisanDES@users.noreply.github.com>
2026-09-18 23:25:00 +0000

    chore: update football data

--- src/generated-jincai-fixtures.js
+++ src/generated-jincai-fixtures.js
@@ -18484,7 +18484,7 @@ window.__JINCAI_DATA__ = {
       }
     }
   ],
-  "enrichedAt": "2026-09-18T21:21:47.215Z",
+  "enrichedAt": "2026-09-18T23:24:59.343Z",
   "enrichment": {
     "providerOverrides": 0,
     "apiFootball": "synced",
