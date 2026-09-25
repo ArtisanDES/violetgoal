56e3380e3e7ec3209b809409d5e4e78e19977154
ArtisanDES <288281471+ArtisanDES@users.noreply.github.com>
2026-09-25 08:05:35 +0000

    chore: update football data

--- src/generated-fixtures.js
+++ src/generated-fixtures.js
@@ -5,7 +5,7 @@ window.__FIXTURES_DATA__ = {
   "dateTo": "2026-07-31",
   "count": 0,
   "matches": [],
-  "enrichedAt": "2026-09-25T02:29:31.537Z",
+  "enrichedAt": "2026-09-25T08:05:34.983Z",
   "enrichment": {
     "providerOverrides": 0,
     "apiFootball": "synced",
